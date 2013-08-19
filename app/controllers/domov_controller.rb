649e3ebd810f8c09ab44fada063d2bc0d6ec40a0
Andraz Kuret <rex007@gmail.com>
2013-08-19 17:42:21 +0200

    some new chancges

--- app/controllers/domov_controller.rb
+++ app/controllers/domov_controller.rb
@@ -2,7 +2,7 @@ class DomovController < ApplicationController
   require 'youtube_it'
 
   def index
-  	@blogs = Blog.limit(2).sort {|a,b| b<=>a}
+  	@blogs = Blog.order("created_at DESC").limit(2)
   	@videos = yt_client.my_videos
   end
 end
